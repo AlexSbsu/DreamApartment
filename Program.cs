641c2de030cd116fca2ebd39bcaa5c269cf33560
alex s <bsu_alexs@mail.ru>
2023-01-27 13:56:53 +0300

    Создана структура программы для первой итерации

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DreamDepartment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
